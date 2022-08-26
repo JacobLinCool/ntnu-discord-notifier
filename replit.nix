7e8f9679faf39f2d1fc2fcca21108c5306f9a728
JacobLinCool <jacoblincool@gmail.com>
2022-08-26 17:48:54 +0000

    feat: add replit support

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+        pkgs.esbuild
+        pkgs.nodejs-18_x
+        pkgs.nodePackages.pnpm
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+    ];
+}
