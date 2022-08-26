34778ae98dc15af21499c31c77893adf9753d3e5
JacobLinCool <jacoblincool@gmail.com>
2022-08-27 01:11:32 +0800

    feat: one shot script

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig(() => ({
-    entry: ["src/index.ts"],
+    entry: ["src/*.ts"],
     outDir: "dist",
     target: "node18",
     format: ["cjs"],
     clean: true,
     splitting: false,
     minify: false,
+    bundle: false,
 }));
